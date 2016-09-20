431ef88da0ff38bb55cf8d718a1c0c6e73f183bc
Roman Boboshko <romanboboshko89@gmail.com>
2016-09-20 19:05:12 +0300

    Add real action to work with API - load courses success

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,8 +1,19 @@
 import * as types from './actionTypes';
+import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
+export function loadCoursesSuccess(courses) {
   return {
-    type: types.CREATE_COURSE,
-    course
+    type: types.LOAD_COURSES_SUCCESS,
+    courses
   };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses))
+    }).catch(error => {
+      throw(error);
+    });
+  }
 }
