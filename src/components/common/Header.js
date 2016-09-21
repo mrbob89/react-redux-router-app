72b0091456a48e80e25d969e4330258b69861ef4
Roman Boboshko <romanboboshko89@gmail.com>
2016-09-21 17:50:01 +0300

    Show saving progress and disable button

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = () => {
+const Header = ({loading}) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -10,9 +10,13 @@ const Header = () => {
       <Link to="/about" activeClassName="active">About</Link>
       {" | "}
       <Link to="/courses" activeClassName="active">Courses</Link>
-      <LoadingDots interval={100} dots={20}></LoadingDots>
+      {loading && <LoadingDots interval={100} dots={20}></LoadingDots>}
     </nav>
   );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -10,7 +10,8 @@ class ManageCoursePage extends Component {
 
     this.state = {
       course: Object.assign({}, this.props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
 
     this.updateCourseState = this.updateCourseState.bind(this);
@@ -33,7 +34,13 @@ class ManageCoursePage extends Component {
 
   saveCourse(event) {
     event.preventDefault();
-    this.props.actions.saveCourse(this.state.course);
+    this.setState({saving: true});
+    this.props.actions.saveCourse(this.state.course)
+      .then(() => this.redirect());
+  }
+
+  redirect() {
+    this.setState({saving: false});
     this.context.router.push('/courses');
   }
 
@@ -45,6 +52,7 @@ class ManageCoursePage extends Component {
         onSave={this.saveCourse}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
       />
     );
   }
