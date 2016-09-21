5b3f7f90bc7e48a109516456c258be0f72442270
Roman Boboshko <romanboboshko89@gmail.com>
2016-09-21 12:39:37 +0300

    Create dump components for courses view + work with Mock API

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
   constructor(props, context) {
@@ -15,10 +16,12 @@ class CoursesPage extends Component {
   }
 
   render() {
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses}/>
       </div>
     );
   }
