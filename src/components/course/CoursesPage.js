166f5d605663d9f5cd62f8f9f6a6cdfc33955141
Roman Boboshko <romanboboshko89@gmail.com>
2016-09-21 16:48:41 +0300

    Handle creates and updates in reducer + dispatch create and update

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,10 +3,13 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
@@ -15,12 +18,22 @@ class CoursesPage extends Component {
     </div>
   }
 
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
+
   render() {
     const {courses} = this.props;
 
     return (
       <div>
         <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}
+        />
         <CourseList courses={courses}/>
       </div>
     );
