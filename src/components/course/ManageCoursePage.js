39b7e28a0a9cfe2ae72945768c87456051458c55
Roman Boboshko <romanboboshko89@gmail.com>
2016-09-21 13:20:49 +0300

    Add manage course page + add routes

---
+++ src/components/course/ManageCoursePage.js
@@ -0,0 +1,35 @@
+import React, {PropTypes, Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions';
+
+class ManageCoursePage extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+          <h1>Manage Course</h1>
+        );
+    }
+}
+
+ManageCoursePage.propTypes
+{
+    //myProp: PropTypes.string.isRequired
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        state: state
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
